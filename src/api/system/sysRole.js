042a55f0f6a4de61a3e5135d0d53a6944ba7b7a2
shellmay <1147258759@qq.com>
2022-11-15 00:41:18 +0800

    角色权限管理

--- src/api/system/sysRole.js
+++ src/api/system/sysRole.js
@@ -64,5 +64,21 @@ export default {
       data: idList
     })
   },
+  //根据用户id查询用户已分配的角色
+  getRolesByUserId(userId) {
+    return request({
+      url: `${api_name}/toAssign/${userId}`,
+      method: 'get'
+  })
+  },
+
+//分配角色
+  assignRoles(assginRoleVo) {
+    return request({
+      url: `${api_name}/doAssign`,
+      method: 'post',
+      data: assginRoleVo
+  })
+  }
 
 }
--- src/api/system/sysUser.js
+++ src/api/system/sysUser.js
@@ -38,5 +38,12 @@ export default {
       url: `${api_name}/remove/${id}`,
       method: 'delete'
     })
+  },
+  //更新用户状态
+  updateStatus(id, status) {
+    return request({
+      url: `${api_name}/updateStatus/${id}/${status}`,
+      method: 'get'
+    })
   }
 }
